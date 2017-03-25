a968510f17f60ba72a4e1a572cb0ae276e509351
dipesh <dipesh.singh2012@gmail.com>
2017-03-25 15:04:47 +0530

    Use standardjs style

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-'use strict';
+'use strict'
 
-exports.register = register;
+exports.register = register
 exports.register.attributes = {
-    name: 'route-prefix-decorator',
-    version: '1.0.0'
-};
-
-function register (server, options, next) {
-    server.decorate('server', 'routePrefix', function (prefix) {
-        var originalRoute = this.route;
-
-        return function (options) {
-            options = [].concat(options);
-
-            for (var i = 0; i < options.length; ++i) {
-                options[i].path = prefix + options[i].path;
-            }
-
-            return originalRoute.call(server, options);
-        };
-    });
-    next();
+  name: 'route-prefix-decorator',
+  version: '1.0.0'
 }
 
+function register (server, options, next) {
+  server.decorate('server', 'routePrefix', function (prefix) {
+    var originalRoute = this.route
+
+    return function (options) {
+      options = [].concat(options)
+
+      for (var i = 0; i < options.length; ++i) {
+        options[i].path = prefix + options[i].path
+      }
+
+      return originalRoute.call(server, options)
+    }
+  })
+  next()
+}
